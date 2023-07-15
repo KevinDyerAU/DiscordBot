e863009d536a4716eeba650b6925abde8a1971ab
KevinDyerAU <kdyer1000@gmail.com>
2023-07-15 18:39:31 +1000

    Updates
    Updates

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import discord
 
 import openai
 
-BOT_TOKEN = "MTEyNjczMTA3Mjc0MzQyMDA0NA.GAnYS9.2b9zPBOlzN2XZhrR2460EntzliNuUyHpkYTUP8"
+BOT_TOKEN = ""
 CHANNEL_ID = 1126741465893175336
 
-OPENAPI_API_KEY = "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"
+OPENAPI_API_KEY = ""
 openai.api_key=OPENAPI_API_KEY
 
 
@@ -16,7 +16,7 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print("Hello! Study bot is ready!")
     channel = bot.get_channel(CHANNEL_ID)
-    await channel.send("Hello! How can I help you tdoay?")
+    await channel.send("Hello! UltronAI bot is ready!")
 
 @bot.event
 async def on_message(message):
@@ -33,4 +33,5 @@ async def on_message(message):
 
     await message.channel.send(response.choices[0].message.content)
 
-bot.run(os.environ["DISCORD_TOKEN"])
+
+bot.run(BOT_TOKEN)
